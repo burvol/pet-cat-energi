d4302605171254a58ac967fd329481301b962c07
Volodymyr Burkovskiy <volodymyr.b.pro@gmail.com>
2021-04-07 00:11:43 +0300

    Видаляю лишню папку.

--- for-github-pages/cat-enargi/js/script.js
+++
@@ -1,8 +0,0 @@
-const mainNavBtn = document.querySelector('.main-nav__toggle');
-const mainNav = document.querySelector('.main-nav');
-
-mainNavBtn.addEventListener('click', () => {
-    console.log('click!');
-    mainNav.classList.toggle('main-nav--opened')
-})
-
